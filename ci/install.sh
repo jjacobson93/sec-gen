fae0cb622dd8747adeac0608d8429874deb5d657
Jeremy Jacobson <jjacobson93@gmail.com>
2018-08-10 22:00:38 -0700

    Add CI config.

---
+++ ci/install.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+set -ex
+
+install_c_toolchain() {
+    case $TARGET in
+        aarch64-unknown-linux-gnu)
+            sudo apt-get install -y --force-yes --no-install-recommends \
+                 gcc-aarch64-linux-gnu libc6-arm64-cross libc6-dev-arm64-cross
+            ;;
+        *)
+            # For other targets, this is handled by addons.apt.packages in .travis.yml
+            ;;
+    esac
+}
+
+install_rustup() {
+  curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=stable
+
+  rustc -V
+  cargo -V
+}
+
+install_openwrt_sdk() {
+  local root_url="https://downloads.openwrt.org/snapshots/trunk/malta/generic/"
+  local tarball=$(curl $root_url  | grep SDK | cut -d'"' -f2)
+  local filename="${tarball%.*}"
+  filename="${filename%.*}"
+
+  mkdir $STAGING_DIR
+  curl -sL "${root_url}/${tarball}" | \
+    tar --strip-components 2 -C $STAGING_DIR -xj "${filename}/staging_dir"
+
+  PATH="$PATH:$(find $STAGING_DIR -maxdepth 1 -name 'toolchain*' -print -quit)/bin" \
+    mipsel-openwrt-linux-gcc -v
+}
+
+install_macos_sdk() {
+  local tarball="MacOSX10.11.sdk.tar.xz"
+  local sdk_url="https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/${tarball}"
+  local osxcross_url="https://github.com/tpoechtrager/osxcross"
+  git clone "${osxcross_url}"
+  cd osxcross
+  curl -sL "${sdk_url}" -o "${tarball}"
+  mv "${tarball}" tarballs/
+  sed -i -e 's|-march=native||g' build_clang.sh wrapper/build.sh
+  UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
+  mkdir -p "$MACOS_SDK_DIR"
+  mv target/* "$MACOS_SDK_DIR"
+}
+
+main() {
+  install_c_toolchain
+  # install_rustup
+  if [ "$TARGET" = "mipsel-unknown-linux-musl" ]; then
+    install_openwrt_sdk
+  fi
+
+  # if [ "$TARGET" = "x86_64-apple-darwin" ]; then
+  #   install_macos_sdk
+  # fi
+}
+
+main
---
+++ ci/script.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+set -ex
+
+main() {
+  install_standard_crates
+  configure_cargo
+  build_cargo_project
+}
+
+install_standard_crates() {
+  rustup target add $TARGET
+}
+
+configure_cargo() {
+  local prefix
+
+  case "$TARGET" in
+    aarch64-unknown-linux-gnu)
+      prefix=aarch64-linux-gnu
+      ;;
+    arm*-unknown-linux-gnueabihf)
+      prefix=arm-linux-gnueabihf
+      ;;
+    arm-unknown-linux-gnueabi)
+      prefix=arm-linux-gnueabi
+      ;;
+    mipsel-unknown-linux-musl)
+      prefix=mipsel-openwrt-linux
+      ;;
+    x86_64-pc-windows-gnu)
+      prefix=x86_64-w64-mingw32
+      ;;
+    x86_64-apple-darwin)
+      prefix=x86_64-apple-darwin15
+    *)
+      return
+      ;;
+  esac
+
+  mkdir -p ~/.cargo
+
+  cat >>~/.cargo/config <<EOF
+[target.$TARGET]
+linker = "$prefix-gcc"
+ar = "$prefix-ar"
+EOF
+}
+
+build_cargo_project() {
+  cargo build --release --target $TARGET
+  mv target/$TARGET/sec-gen binaries/sec-gen-$TARGET
+  md5sum binaries/sec-gen-$TARGET > binaries/sec-gen-$TARGET.md5
+  sha256sum binaries/sec-gen-$TARGET > binaries/sec-gen-$TARGET.sha256
+}
+
+main
